047506abddb5f8e36e7da917167205d62e0c5bcc
iamnotafishzzz <576945179@qq.com>
2017-01-05 21:47:39 +0800

    Add codes of auto-generating ensemble members (ver2)

--- testClustering.py
+++
@@ -1,5 +0,0 @@
-import basicClusterMethods as bCM
-import dataSetPreprocessing as dataPre
-
-dataSet, target = dataPre.loadRobotExecution_1()
-print (bCM.KMeans_c(dataSet))
--- testfile_lzj.py
+++ testfile_lzj.py
@@ -2,8 +2,8 @@ import dataSetPreprocessing as dSP
 import ensemble_generation as eg
 
 dataSets = {'digit': dSP.loadDigits, 'movement': dSP.loadMovement_libras}
-paramSettings = {'digit': {'members': 160, 'classnum': 10},
-                 'movement': {'members': 160, 'classnum': 15}}
+paramSettings = {'digit': {'members': 160, 'classNum': 10},
+                 'movement': {'members': 160, 'classNum': 15}}
 
 eg.autoGenerationWithConsensus(dataSets, paramSettings)
 
