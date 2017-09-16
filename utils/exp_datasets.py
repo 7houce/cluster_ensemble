1022248cb813e1a91a4840858b6f6a7e95f8644c
emrlay <zjlin@live.com>
2017-09-16 13:18:18 +0800

    dataset information added.

--- utils/exp_datasets.py
+++ utils/exp_datasets.py
@@ -1,3 +1,15 @@
 import utils.load_dataset as ld
 
-dataset = {'OPTDIGITS': {'k': 10, 'data': ld.load_digit}}
+dataset = {
+    'OPTDIGITS': {'k': 10, 'data': ld.load_digit},
+    'Segmentation': {'k': 7, 'data': ld.load_segmentation},
+    'ISOLET': {'k': 26, 'data': ld.loadIsolet},
+    'MNIST4000': {'k': 10, 'data': ld.load_mnist_4000},
+    'COIL20': {'k': 20, 'data': ld.load_coil20},
+    'DLBCL': {'k': 2, 'data': ld.load_DLBCL},
+    'Prostate': {'k': 2, 'data': ld.load_Prostate_Tumor},
+    'SRBCT': {'k': 4, 'data': ld.load_SRBCT},
+    'LungCancer': {'k': 4, 'data': ld.load_Lung_Cancer},
+    'Leukemia1': {'k': 3, 'data': ld.load_Leukemia1},
+    'Leukemia2': {'k': 3, 'data': ld.load_Leukemia2}
+}
